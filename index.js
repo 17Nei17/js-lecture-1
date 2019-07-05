2b6ec7d7805dca3973ecff5e18e43dce9f96e9de
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-07-05 16:18:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// index.js
+//console.log('hello world!');
+const http = require ('http');
+const fs = require ('fs');
+
+const server = http.createServer(function (request, responce) {
+    console.log(request.method, request.url);
+    if (request.url == '/'){
+       const content = fs.readFileSync ('index.html','utf8'); 
+       responce.end(content); 
+    }
+    else if (request.url == '/style.css'){
+        const content = fs.readFileSync ('style.css','utf8'); 
+        responce.end(content); 
+    }
+
+})
+
+server.listen(3000);
+console.log('server started');
